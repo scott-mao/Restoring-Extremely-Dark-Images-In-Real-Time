2c2bda57dcbe9a829d326d9ce1cb990df8c8d662
Mohit Lamba <60866304+MohitLamba94@users.noreply.github.com>
2021-07-05 12:31:11 +0530

    Add files via upload

---
+++ train_test_ours/train_test_SID_CVPR_18/network.py
@@ -0,0 +1,105 @@
+import torch
+import torch.nn as nn
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        
+        self.up2 = nn.PixelShuffle(2)
+        self.lrelu = nn.LeakyReLU(0.2, inplace=False)
+        self.conv1_1 = nn.Conv2d(4, 32, kernel_size=3, stride=1, padding=1)
+        self.conv1_2 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
+        
+        self.conv2_1 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.conv2_2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
+        
+        self.conv3_1 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.conv3_2 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+        self.pool3 = nn.MaxPool2d(kernel_size=2)
+        
+        self.conv4_1 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.conv4_2 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.pool4 = nn.MaxPool2d(kernel_size=2)
+        
+        self.conv5_1 = nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1)
+        self.conv5_2 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        
+        self.upv6 = nn.ConvTranspose2d(512, 256, 2, stride=2)
+        self.conv6_1 = nn.Conv2d(512, 256, kernel_size=3, stride=1, padding=1)
+        self.conv6_2 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        
+        self.upv7 = nn.ConvTranspose2d(256, 128, 2, stride=2)
+        self.conv7_1 = nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1)
+        self.conv7_2 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+        
+        self.upv8 = nn.ConvTranspose2d(128, 64, 2, stride=2)
+        self.conv8_1 = nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1)
+        self.conv8_2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        
+        self.upv9 = nn.ConvTranspose2d(64, 32, 2, stride=2)
+        self.conv9_1 = nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1)
+        self.conv9_2 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
+        
+        self.conv10_1 = nn.Conv2d(32, 12, kernel_size=1, stride=1)
+    
+    def forward(self, x):
+        conv1 = self.lrelu(self.conv1_1(self.downshuffle(x,2)))
+        conv1 = self.lrelu(self.conv1_2(conv1))
+        pool1 = self.pool1(conv1)
+        
+        conv2 = self.lrelu(self.conv2_1(pool1))
+        conv2 = self.lrelu(self.conv2_2(conv2))
+        pool2 = self.pool1(conv2)
+        
+        conv3 = self.lrelu(self.conv3_1(pool2))
+        conv3 = self.lrelu(self.conv3_2(conv3))
+        pool3 = self.pool1(conv3)
+        
+        conv4 = self.lrelu(self.conv4_1(pool3))
+        conv4 = self.lrelu(self.conv4_2(conv4))
+        pool4 = self.pool1(conv4)
+        
+        conv5 = self.lrelu(self.conv5_1(pool4))
+        conv5 = self.lrelu(self.conv5_2(conv5))
+        
+        up6 = self.upv6(conv5)
+        up6 = torch.cat([up6, conv4], 1)
+        conv6 = self.lrelu(self.conv6_1(up6))
+        conv6 = self.lrelu(self.conv6_2(conv6))
+        
+        up7 = self.upv7(conv6)
+        up7 = torch.cat([up7, conv3], 1)
+        conv7 = self.lrelu(self.conv7_1(up7))
+        conv7 = self.lrelu(self.conv7_2(conv7))
+        
+        up8 = self.upv8(conv7)
+        up8 = torch.cat([up8, conv2], 1)
+        conv8 = self.lrelu(self.conv8_1(up8))
+        conv8 = self.lrelu(self.conv8_2(conv8))
+        
+        up9 = self.upv9(conv8)
+        up9 = torch.cat([up9, conv1], 1)
+        conv9 = self.lrelu(self.conv9_1(up9))
+        conv9 = self.lrelu(self.conv9_2(conv9))
+        
+        conv10= self.conv10_1(conv9)
+        out = self.up2(conv10)
+        return out
+    
+    def downshuffle(self,var,r):
+        b,c,h,w = var.size()
+        out_channel = c*(r**2)
+        out_h = h//r
+        out_w = w//r
+        return var.contiguous().view(b, c, out_h, r, out_w, r).permute(0,1,3,5,2,4).contiguous().view(b,out_channel, out_h, out_w).contiguous()
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                m.weight.data.normal_(0.0, 0.02)
+                if m.bias is not None:
+                    m.bias.data.normal_(0.0, 0.02)
+            if isinstance(m, nn.ConvTranspose2d):
+                m.weight.data.normal_(0.0, 0.02)
---
+++ train_test_ours/train_test_SID_CVPR_18/train.py
@@ -0,0 +1,126 @@
+opt={'base_lr':1e-4}
+
+
+opt['batch_size'] = 1
+opt['atWhichSave'] = [2,100002,150002,200002,250002,300002,350002,400002,450002,500002,550000, 600000,650002,700002,750000,800000,850002,900002,950000,1000000]
+opt['iterations'] = 1000005
+dry_run_iterations = 100
+dry_run = False
+metric_average_file = 'metric_average.txt'
+test_amplification_file = 'test_amplification.txt'
+train_amplification_file = 'train_amplification.txt'
+# These are folders
+save_weights = 'weights'
+save_images = 'images' # make this change below also
+save_csv_files = 'csv_files'
+
+import numpy as np
+import os
+import shutil
+import torch
+from torch.utils.data import DataLoader
+import torch.optim as optim
+import glob
+
+from common_classes import load_data, run_test
+from network import Net
+
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
+
+shutil.rmtree(metric_average_file, ignore_errors = True)
+shutil.rmtree(test_amplification_file, ignore_errors = True)
+shutil.rmtree(train_amplification_file, ignore_errors = True)
+
+shutil.rmtree(save_weights, ignore_errors = True)
+shutil.rmtree(save_images, ignore_errors = True)
+shutil.rmtree(save_csv_files, ignore_errors = True)
+
+os.makedirs(save_weights)
+os.makedirs(save_images)
+os.makedirs(save_csv_files)
+
+train_files = glob.glob('/SID_cvpr_18_dataset/Sony/short/0*_00_0.1s.ARW')
+train_files +=glob.glob('/SID_cvpr_18_dataset/Sony/short/2*_00_0.1s.ARW')
+if dry_run:
+    train_files = train_files[:2]
+    opt['iterations'] = dry_run_iterations
+    
+gt_files = []
+for x in train_files:
+    gt_files += glob.glob('/SID_cvpr_18_dataset/Sony/long/*'+x[-17:-12]+'*.ARW')
+    
+dataloader_train = DataLoader(load_data(train_files,gt_files,train_amplification_file,20,gt_amp=True,training=True), batch_size=opt['batch_size'], shuffle=True, num_workers=0, pin_memory=True)
+
+test_files = glob.glob('/media/data/mohit/chen_dark_cvpr_18_dataset/Sony/short/1*_00_0.1s.ARW') 
+if dry_run:
+    test_files = test_files[:2]
+    
+gt_files = []
+for x in test_files:
+    gt_files = gt_files+ glob.glob('/media/data/mohit/chen_dark_cvpr_18_dataset/Sony/long/*'+x[-17:-12]+'*.ARW')
+dataloader_test = DataLoader(load_data(test_files,gt_files,test_amplification_file,2,gt_amp=True,training=False), batch_size=1, shuffle=False, num_workers=0, pin_memory=True)
+
+for i,img in enumerate(dataloader_train):    
+    print('Input image size : {}, GT image size : {}'.format(img[0].size(), img[1].size()))    
+    break
+    
+############ Training Begins
+
+device = torch.device("cuda")
+model = Net()
+print(model)
+print('\nTrainable parameters : {}\n'.format(sum(p.numel() for p in model.parameters() if p.requires_grad)))
+model = model.to(device)
+print('Device on cuda: {}'.format(next(model.parameters()).is_cuda))
+
+iter_num = 0
+l1_loss = torch.nn.L1Loss()
+
+optimizer = torch.optim.Adam(model.parameters(), lr=opt['base_lr'])
+optimizer.zero_grad()
+loss_list = ['L1_loss']
+loss_iter_list = ['Iteration']
+iter_LR = ['Iter_LR']
+            
+
+while iter_num<opt['iterations']:
+    for _, img in enumerate(dataloader_train):
+        low = img[0].to(device)
+        gt = img[1].to(device)
+        model.train()
+        pred = model(low)
+        iter_num +=1
+        #loss2 = feature_loss(pred, gt, which='relu2')
+        loss1 = l1_loss(pred,gt)
+        #print('l1,vgg loss: ',loss1.item(), loss2.item())
+        loss = loss1
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()        
+        
+        if iter_num>opt['iterations']:
+            break        
+        
+        if iter_num%10==0:
+            print(iter_num)
+            if iter_num%100==0:
+                loss_list.append('{},{},{}'.format(loss1.item(),-1,-1))
+                loss_iter_list.append(iter_num)
+                iter_LR.append(optimizer.param_groups[0]['lr'])
+                
+        if iter_num in opt['atWhichSave']:
+            print('testing......')
+            if iter_num == opt['atWhichSave'][0]:
+                mode = 'w'
+            else:
+                mode = 'a'
+            run_test(model, dataloader_test, iter_num, save_images, save_csv_files, metric_average_file, mode, training=True)
+            torch.save({'model': model.state_dict()},os.path.join(save_weights,'weights_{}'.format(iter_num)))
+
+
+np.savetxt(os.path.join(save_csv_files,'loss_curve.csv'),[p for p in zip(loss_iter_list,loss_list,iter_LR)],delimiter=',',fmt='%s')
+
+   
+                
+        
